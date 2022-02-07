3bcdc9f675268be1b44eacb8ecc22d4b487f8c2e
arghya-roy <95282173+arghya-roy@users.noreply.github.com>
2022-02-08 02:53:11 +0530

    Update instance.tf

--- demo-8/instance.tf
+++ demo-8/instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   subnet_id = aws_subnet.main-public-1.id
 
   # the security group
-  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]  # it is under third bracket means it is a array, but in this example there is only one value.
 
   # the public SSH key
   key_name = aws_key_pair.mykeypair.key_name
